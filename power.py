a148b8d0073c00aa0ae0497a0d3d414fa1b15230
isai-ledezma <56975661+isai-ledezma@users.noreply.github.com>
2021-06-15 08:44:25 -0600

    Create power.py

---
+++ power.py
@@ -0,0 +1,9 @@
+import math
+
+
+def main(number, potence):
+    print(math.pow(number, potence))
+
+
+if __name__ == '__main__':
+    main(float(input('give me the number:')), float(input('give me the potence')))
