4bcdeb2abae844605c1c3538d1b777b3edcafcb2
isai-ledezma <56975661+isai-ledezma@users.noreply.github.com>
2021-06-23 13:39:57 -0600

    Update power.py

--- power.py
+++ power.py
@@ -1,9 +1,22 @@
-import math
+from math import pow
 
 
-def main(number, potence):
-    print(math.pow(number, potence))
+def potence(n, *args):
+    n2 = args[0]
+    if args:
+        print(pow(n, n2))
+    else:
+        print(pow(n, 2))
+
+
+def main():
+    n = int(input('give me the number:'))
+    n2 = input('give me the power or not, is optional:')
+    if n2 == '':
+        potence(n)
+    else:
+        potence(n, int(n2))
 
 
 if __name__ == '__main__':
-    main(float(input('give me the number:')), float(input('give me the potence')))
+    main()
